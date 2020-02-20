d6521e07295b9a4fbdbc5ca4730dd8f7d8ca1331
Hien Le <hienle@askaborg.com>
2020-02-20 03:52:37 -0800

    Team Up

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+import Form from './team/Form'
+import PlayerList from './team/PlayerList'
 
 function App() {
+  const[teamPlayer, addTeamPlayer] = useState([])
+  const teamUp = teamPlayerList => addTeamPlayer([...teamPlayer, teamPlayerList])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form teamUp={teamUp} />
+      <PlayerList teamPlayer={teamPlayer} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
